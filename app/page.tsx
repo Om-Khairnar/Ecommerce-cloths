330d99888cff39038166b0dcf0df892f3f108bad
Om-khairnar <om.khairnar175@gmail.com>
2024-07-02 11:19:00 +0530

    solving error of the sanity private 1

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Hero from "./components/Hero";
 import Newest from "./components/Newest";
 import Slider from "./components/Slider";
 
+export const dynamic = " force-dynamic";
+
+
 export default function Home() {
   return (
     <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
